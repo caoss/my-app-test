5ef9564d535fd855ea7c70cfaebc418c177e9dac
caoss <951152901@qq.com>
2020-04-10 15:16:05 +0800

    css

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
 import React, { Component, Fragment } from "react";
-import { Provider } from "react-redux";
 import { BrowserRouter, Route } from "react-router-dom";
 import Home from "./pages/About";
 import Test from "./pages/Test";
-import store from "./store";
 
 class App extends Component {
     render() {
         return (
-            <Provider store={store}>
-                <Fragment>
-                    <BrowserRouter>
-                        <div>
-                            <Route path='/' exact component={Home}></Route>
-                            <Route path='/test' exact component={Test}></Route>
-                        </div>
-                    </BrowserRouter>
-                </Fragment>
-            </Provider>
+            <Fragment>
+                <BrowserRouter>
+                    <div>
+                        <Route path='/' exact component={Home}></Route>
+                        <Route path='/test' exact component={Test}></Route>
+                    </div>
+                </BrowserRouter>
+            </Fragment>
         );
     }
 }
