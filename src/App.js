7b34647199c1cd0520f6107ea709210b20744f00
caoss <951152901@qq.com>
2020-04-10 09:15:09 +0800

    css

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Component, Fragment } from "react";
 import { Provider } from "react-redux";
 import { BrowserRouter, Route } from "react-router-dom";
 import Home from "./pages/About";
+import Test from "./pages/Test";
 import store from "./store";
 
 class App extends Component {
@@ -12,6 +13,7 @@ class App extends Component {
                     <BrowserRouter>
                         <div>
                             <Route path='/' exact component={Home}></Route>
+                            <Route path='/test' exact component={Test}></Route>
                         </div>
                     </BrowserRouter>
                 </Fragment>
